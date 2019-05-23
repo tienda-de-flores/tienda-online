2313687d304e53b1172a31935e3d8a1018149b06
usagui <danita9516@gmail.com>
2019-05-23 16:47:41 -0400

    penultimas mejoras catalogo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path:'store',
+    path:'catalogo',
     loadChildren: './store/store.module#StoreModule'
   }
 ];
